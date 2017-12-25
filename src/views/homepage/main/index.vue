9c2da91501b941f553af7c594c3ec29c9a15e33f
Jaked <530974640@qq.com>
2017-12-25 11:44:25 +0800

    update package.json

--- src/views/homepage/main/index.vue
+++ src/views/homepage/main/index.vue
@@ -1,23 +1,6 @@
 <template>
-    <div class="home">
-        <img src="~assets/logo.png">
-        <h1>hello</h1>
-        <div class="flex-wrapper">
-            <div class="flex-item" @click="tap">
-                flex-item1
-                <img src="~assets/icon-eye-close.png" />
-            </div>
+    <div class="homepage">
 
-            <div class="flex-item" @click="tap">
-                flex-item2
-                <img src="~assets/icon-eye-open.png" />
-            </div>
-            <div class="flex-item" @click="tap">
-                flex-item3
-                <img src="~assets/icon-arrow-rt.png" />
-            </div>
-        </div>
-        <img src="~assets/test2.png" class="img-responsive" />
     </div>
 </template>
 <style lang="scss" src="./style.scss"></style>
