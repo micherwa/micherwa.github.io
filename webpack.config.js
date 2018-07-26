082cf35cece41472b6ad925a047f30d177d09854
Jaked <530974640@qq.com>
2018-07-26 17:52:18 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var config = {
         path: path.resolve(__dirname, './blog'),
         filename: '[name].[chunkhash].js',
         // 打包后，对于根目录下的index.html，须配置绝对引用路径
-        // publicPath: isProd ? '/blog' : '/'
+        publicPath: isProd ? '/blog' : '/'
     },
     module: {
         rules: [
