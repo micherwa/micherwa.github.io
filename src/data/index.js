18ce0374958fc31c8f37c755f84a0dd942937324
Jaked <530974640@qq.com>
2018-07-30 11:55:34 +0800

    no message

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,9 @@
 
 export default [
     {
-        title: '饿了么的 PWA 升级实践',
+        title: 'Test title',
         routeName: 'upgrading-eleme-to-pwa',
-        descript: '很荣幸在今年 2 月到 5 月的时间里，以顾问的身份加入饿了么，参与 PWA 的相关工作。这篇文章其实最初是在以英文写作发表在 medium 上的：Upgrading Ele.me to Progressive Web Apps，获得了一定的关注。所以也...',
+        descript: 'test description...',
         date: '2017-07-21'
     }
 ];
