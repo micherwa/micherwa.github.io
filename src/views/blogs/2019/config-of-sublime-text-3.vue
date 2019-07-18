c4962d71f33d2c79b07e9732d5de568afe648221
Jaked <530974640@qq.com>
2019-07-18 19:01:24 +0800

    no message

--- src/views/blogs/2019/config-of-sublime-text-3.vue
+++ src/views/blogs/2019/config-of-sublime-text-3.vue
@@ -6,9 +6,19 @@
             <div slot="content">
                 <h2>前言</h2>
                 <p>
-
+                    今天为大家分享一款我用了很久的编辑器：sublime text 3。它的轻便和高度可定制化，是我觉得最棒的地方。本文会涉及到 sublime text 3 的方方面面，方便自己的同时，也希望能帮到你。
                 </p>
 
+                <h2>安装与初始化</h2>
+                <p>
+                    可以在
+                    <a href="http://www.sublimetext.com/3" target="_blank">官网</a>
+                    下载最新版的安装文件。选择对应的平台，我用的 mac 版，最新的安装包也才 15.2 M，真的很轻量了。
+                </p>
+
+                <p>
+                    安装完成后的第一件事，就是按照自己的习惯进行定制。打开
+                </p>
             </div>
         </BlogContent>
     </div>
